05917798fc040250dc7866117bbd9e0404366ea4
MomoxStudios <momoxstudios@gmail.com>
2020-11-12 00:58:13 +0100

    Update sv_updater.lua
    Removed Debug prints, fixed updates not working properly, added loggig support, added more detailed error messages, increased update check frequency

--- lua/ipgb/sv_updater.lua
+++ lua/ipgb/sv_updater.lua
@@ -7,22 +7,31 @@ Note: You aren't supposed to change anything in here unless you know what to do!
 IPGB.Core = IPGB.Core or {}
 IPGB.Util = IPGB.Util or {}
 
-IPGB.Core.CurrentVersion = "1.1.0"
+IPGB.Core.CurrentVersion = 110
 
-function IPGB.Util.UpdateCheck(ply)
+function IPGB.Util.UpdateCheck()
 	http.Fetch( "https://raw.githubusercontent.com/MomoxStudios/IPGBlite/master/version.txt" , function(body, len, headers, code)	
-
-				print(type(body),body," | ",type(IPGB.Core.CurrentVersion),IPGB.Core.CurrentVersion)
-
-				if body != IPGB.Core.CurrentVersion then
-					MsgC( Color( 255, 0, 0 ), "[IPGB] WARNING: You are using an outdated version. Please update from version [" .. IPGB.Core.CurrentVersion .. "] to version [" .. body .. "].\n")
-					IPGB.Core.IsOutdated = true
+			if len < 6 then
+				if IPGB.Core.CurrentVersion == tonumber(body) then
+					MsgC( Color( 20, 255, 20 ), "[IPGB] Version Up-To-Date!\n")
 				else
-					MsgC( Color( 20, 255, 20 ), "[IPGB] Version Up-To-Date: version: " .. body .. "\n")
-				end			
+					MsgC( Color( 255, 0, 0 ), "[IPGB] WARNING: You are using an outdated version. Please update from version [" .. IPGB.Core.CurrentVersion .. "] to version [" .. tonumber(body) .. "].\n")
+					if (IPGB.LogInstalled) then
+		                IPGB.Util.LogError("WARNING: You are using an outdated version. Please update from version [" .. IPGB.Core.CurrentVersion .. "] to version [" .. tonumber(body) .. "].")
+		            end
+				end	
+			else
+				MsgC( Color( 255, 0, 0 ), "[IPGB] UPDATE ERROR: GitHub sent invalid data.\n")
+			 if (IPGB.LogInstalled) then
+                IPGB.Util.LogError("ERROR: Couldn't check for updates. GitHub sent invalid data" )
+            end	
+			end			
 		end,
 		function( error )
-			MsgC( Color( 255, 0, 0 ), "[IPGB] FATAL ERROR: Connection to GitHub failed.\n")
+			MsgC( Color( 255, 0, 0 ), "[IPGB] UPDATE ERROR: Connection to GitHub failed.\n")
+			 if (IPGB.LogInstalled) then
+                IPGB.Util.LogError("ERROR: Couldn't check GitHub for a newer version: " .. error )
+            end
 		end
 	)
 	return true
