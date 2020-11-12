ab1254c5400ebfd53416389f5fdf7fb0d63833ba
MomoxStudios <momoxstudios@gmail.com>
2020-11-12 14:09:12 +0100

    Update sv_updater.lua
    Update check now works through JSON instead of txt, also including a small changelog now.

--- lua/ipgb/sv_updater.lua
+++ lua/ipgb/sv_updater.lua
@@ -7,17 +7,18 @@ Note: You aren't supposed to change anything in here unless you know what to do!
 IPGB.Core = IPGB.Core or {}
 IPGB.Util = IPGB.Util or {}
 
-IPGB.Core.CurrentVersion = 110
+IPGB.Core.CurrentVersion = "1.0.0"
 
 function IPGB.Util.UpdateCheck()
-	http.Fetch( "https://raw.githubusercontent.com/MomoxStudios/IPGBlite/master/version.txt" , function(body, len, headers, code)	
-			if len < 6 then
-				if IPGB.Core.CurrentVersion == tonumber(body) then
+	http.Fetch( "https://raw.githubusercontent.com/MomoxStudios/ipgblite/master/version.json" , function(body, len, headers, code)	
+		local content = util.JSONToTable(body) or "ERROR"
+			if content != "ERROR" then
+				if IPGB.Core.CurrentVersion == content.version then
 					MsgC( Color( 20, 255, 20 ), "[IPGB] Version Up-To-Date!\n")
 				else
-					MsgC( Color( 255, 0, 0 ), "[IPGB] WARNING: You are using an outdated version. Please update from version [" .. IPGB.Core.CurrentVersion .. "] to version [" .. tonumber(body) .. "].\n")
+					MsgC( Color( 255, 0, 0 ), "[IPGB] WARNING: Update available! Version [" .. IPGB.Core.CurrentVersion .. "] -> [" .. content.version .. "]. Changes: " .. content.changes.. "\n")
 					if (IPGB.LogInstalled) then
-		                IPGB.Util.LogError("WARNING: You are using an outdated version. Please update from version [" .. IPGB.Core.CurrentVersion .. "] to version [" .. tonumber(body) .. "].")
+		                IPGB.Util.LogError("WARNING: Update available! Version [" .. IPGB.Core.CurrentVersion .. "] -> [" .. content.version .. "]. Changes: " .. content.changes)
 		            end
 				end	
 			else
